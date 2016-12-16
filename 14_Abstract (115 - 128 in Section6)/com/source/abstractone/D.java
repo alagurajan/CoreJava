be5a8c2a60aac072f777d8b0320e4c94a1266cb3
bala <kingboons@gmail.com>
2016-12-17 03:54:58 +0530

    read my comment bro

--- 14_Abstract (115 - 128 in Section6)/com/source/abstractone/D.java
+++ 14_Abstract (115 - 128 in Section6)/com/source/abstractone/D.java
@@ -4,3 +4,6 @@ abstract class D
 {
 	//void test1(); 	// At least one abstract method should be in Abstract class		(Uncomment this line)
 }
+
+
+//not necessary. we can declare a abstract class without any abstract method also
